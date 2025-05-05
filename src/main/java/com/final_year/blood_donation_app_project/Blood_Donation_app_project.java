72ede67914eb92a8c09c288dc82fb32425848b9a
Aryan <Aryan@LAPTOP-U83VGKVC>
2025-05-06 00:35:25 +0530

    Initial Commit

---
+++ src/main/java/com/final_year/blood_donation_app_project/Blood_Donation_app_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.final_year.blood_donation_app_project;
+
+/**
+ *
+ * @author Aryan
+ */
+public class Blood_Donation_app_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
